05d481032e4abf495c1b16679c57c6db491b41ae
alokbharti <bhartialok.257@gmail.com>
2020-11-01 09:08:55 +0530

    Added close button in search bar
    - close button will allow user to clear the text and load previous data

--- app/src/main/java/com/example/twittersearchclient/ui/main/MainFragment.kt
+++ app/src/main/java/com/example/twittersearchclient/ui/main/MainFragment.kt
@@ -2,6 +2,8 @@ package com.example.twittersearchclient.ui.main
 
 import android.os.Bundle
 import android.os.Handler
+import android.text.Editable
+import android.text.TextWatcher
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -27,6 +29,8 @@ class MainFragment : Fragment() {
 
     private val viewModel by viewModels<MainViewModel>()
     private var tweetList: ArrayList<Tweet> = ArrayList()
+    /**This will help to restore all tweets if user clears the search text*/
+    private var tweetListCache: ArrayList<Tweet> = ArrayList()
     private lateinit var adapter: TweetAdapter
     private lateinit var binding: MainFragmentBinding
 
@@ -75,19 +79,21 @@ class MainFragment : Fragment() {
                     binding.tvApiState.text = getString(R.string.failed_api_response)
                 }
             } else {
+                tweetListCache.addAll(it)
                 tweetList.addAll(it)
                 adapter.notifyDataSetChanged()
             }
         })
 
         viewModel.getAllFilteredTweetsFromDb().observe(viewLifecycleOwner, Observer {
+            tweetList.clear()
             if (it.isNullOrEmpty()){
-
+                binding.tvApiState.text = getString(R.string.no_tweets_found)
             } else {
-                tweetList.clear()
+                binding.tvApiState.text = ""
                 tweetList.addAll(it)
-                adapter.notifyDataSetChanged()
             }
+            adapter.notifyDataSetChanged()
         })
 
         binding.btnSearch.setOnClickListener{
@@ -95,6 +101,32 @@ class MainFragment : Fragment() {
             if (searchedText.isNotEmpty()){
                 viewModel.searchTweets(searchedText)
             }
+        }
+
+        binding.tetSearch.addTextChangedListener(object : TextWatcher{
+            override fun afterTextChanged(s: Editable?) {
+                val text = s.toString()
+                if (text.isNotEmpty()){
+                    binding.ivCloseSearch.visibility = View.VISIBLE
+                } else {
+                    binding.ivCloseSearch.visibility = View.GONE
+                }
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+        })
+
+        binding.ivCloseSearch.setOnClickListener{
+            binding.tetSearch.setText("")
+            binding.tetSearch.clearFocus()
+            binding.tvApiState.text = ""
+
+            //show all tweets
+            tweetList.clear()
+            tweetList.addAll(tweetListCache)
+            adapter.notifyDataSetChanged()
         }
     }
 
